b7e00f28bd93195908d85b274a4bf93f20c5ec8d
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2015-09-09 14:57:26 +0900

    #127

--- src/for-clojure/repl9.clj
+++ src/for-clojure/repl9.clj
@@ -99,3 +99,113 @@
 
 ; [excellent]
 #(Long/parseLong % 2)
+
+(= {[1 3] #{[1 2]}, [0 2] #{[1 2]}, [3 1] #{[2 1]}, [2 0] #{[2 1]}}
+   (__ '[[e e e e]
+         [e w b e]
+         [e b w e]
+         [e e e e]] 'w))
+
+((fn [board color]
+   (let [MAXL (count board)
+         MAXC (count (first board))
+         COMP ({'b 'w 'w 'b} color)]
+     (letfn [(empties []
+                      (for [i (range MAXL)
+                            j (range MAXC)
+                            :when (= 'e ((board i) j))]
+                        [i j]))
+
+             (flipped-coor [coll]
+                           (let [colors (for [coor coll
+                                              :let [x (first coor)
+                                                    y (second coor)]
+                                              :while (or (= [x y] (first coll))
+                                                         (not= ((board x) y) 'e))
+                                              ]
+                                          ((board x) y))]
+                             (cond
+                               (> 3 (count colors)) nil
+                               (= color (last colors))
+                               (set (take (count (filter #(= COMP %) colors)) (rest coll)))
+                               )))
+
+             (flipped [coor]
+                      (let [x (first coor)
+                            y (second coor)
+                            flipped-coors
+                            (apply clojure.set/union #{}
+                                   (map flipped-coor
+                                        (conj []
+                                              ; line
+                                              (for [j (range y MAXC)] [x j])
+                                              (for [j (reverse (range (inc y)))] [x j])
+                                              ; coll
+                                              (for [i (range x MAXL)] [i y])
+                                              (for [i (reverse (range (inc x)))] [i y])
+                                              ; cross
+                                              (for [i (range MAXC)
+                                                    :let [x' (+ x i)
+                                                          y' (+ y i)]
+                                                    :while (and (< x' MAXL)
+                                                                (< y' MAXC))] [x' y'])
+                                              (for [i (range MAXC)
+                                                    :let [x' (- x i)
+                                                          y' (- y i)]
+                                                    :while (and (>= x' 0)
+                                                                (>= y' 0))] [x' y'])
+                                              (for [i (range MAXC)
+                                                    :let [x' (+ x i)
+                                                          y' (- y i)]
+                                                    :while (and (< x' MAXL)
+                                                                (>= y' 0))] [x' y'])
+                                              (for [i (range MAXC)
+                                                    :let [x' (- x i)
+                                                          y' (+ y i)]
+                                                    :while (and (>= x' 0)
+                                                                (< y' MAXC))] [x' y'])
+                                              )))]
+
+                        (if (not-empty flipped-coors)
+                          [coor flipped-coors]
+                          nil)
+
+                        ))
+             ]
+       (reduce #(conj %1 (apply hash-map %2)) {}
+         (filter (complement nil?)
+                 (for [c (empties)]
+                   (flipped c)))))))
+  '[[e e e e]
+    [e w b e]
+    [w w w e]
+    [e e e e]] 'b
+  )
+
+;(apply hash-map (flatten '((:a :b) (:c :d))))
+
+(
+  ;(str '
+  (fn Quines []
+   (let [q  (char 34)
+         lf (char 32)
+         st [
+             "(fn Quines []"
+             "(let [q (char 34)"
+             "lf (char 32)"
+             "st ["
+             "]]"
+             "(str"
+             "(clojure.string/trim (apply str (for [s (range 4)] (str (st s) lf))))"
+             "(clojure.string/trim (apply str (for [s st] (str q s q lf))))"
+             "(clojure.string/trim (apply str (for [s (range 4 (count st))] (str (st s) lf)))))))"
+             ]]
+     (str
+       (clojure.string/trim (apply str (for [s (range 4)] (str (st s) lf))))
+       (clojure.string/trim (apply str (for [s st] (str q s q lf))))
+       (clojure.string/trim (apply str (for [s (range 4 (count st))] (str (st s) lf)))))))
+  )
+
+; [excellent]
+((fn [] (let [s "(fn [] (let [s %s] (format s (pr-str s))))"] (format s (pr-str s)))))
+(fn [p] (str p p)) '(fn [p] (str p p))
