9c4090817f22427215e9d5220ab1c13f20617bc1
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2015-09-11 19:49:50 +0900

    #127 偶数はできた

--- src/for-clojure/repl10.clj
+++ src/for-clojure/repl10.clj
@@ -44,36 +44,76 @@
 ((fn triangle [coll]
    (let [rocks (let [_rb (for [x coll]
                           (Integer/toBinaryString x))
-                     _maxc (apply max (conj (map count _rb) (count _rb)))
+                     ;正方形桁埋めあり
+;                     _maxc (apply max (conj (map count _rb) (count _rb)))
+                     _maxc (apply max (map count _rb))
+                     _maxl (count _rb)
                      _v (vec (map #(vec (format (str "%0" _maxc "d")
                                             (Integer/parseInt %)))
                               _rb))]
-                 (for [x (range _maxc)]
+                 (for [x (range _maxl)]
                    (for [y (range _maxc)]
                      (if (< x (count _v))
                        (Integer/parseInt (str ((_v x) y)))
                        0))))
+         LOWX (count rocks)
+         COLY (count (first rocks))
 
-         debug (fn [target]
-                 (println "debug::---")
+         ; utils
+         debug (fn _d
+                 ([target] (_d "debug" target))
+                 ([name target]
+                 (println name "::---")
                  (loop [_c target]
                    (println (apply str (first _c)))
                    (if (not-empty _c)
                      (recur (rest _c))
-                     target)))
+                     target))))
 
+         debug-b (fn [b]
+                   (debug "---- shifted"
+                          (partition-all COLY
+                                         (seq (.replace
+                                                (format (str "%" (* LOWX COLY) "s")
+                                                        (Integer/toBinaryString b))
+                                                " " "0"))))
+                   b)
+
+           to-b (fn [coll]
+                (Long/parseLong (apply str (flatten coll)) 2))
+         countm (fn [mine]
+                  (reduce + (filter #(= 1 %) (flatten mine))))
+         padding-y (fn [mask]
+                     (let [n (- COLY (count (first mask)))]
+                       (if (> n 0)
+                         (for [l mask]
+                           (concat l (take n (cycle '(0)))))
+                         mask)))
+         padding-x (fn [mask]
+                     (let [n (- LOWX (count mask))]
+                       (if (> n 0)
+                         (partition-all COLY
+                           (concat (flatten mask)
+                                  (take (* n COLY) (cycle '(0)))))
+                         mask)))
+         padding (fn [mask]
+                   (-> mask
+                       (padding-y)
+                       (padding-x)))
+
+         ; create - triangle
          create-e (fn [size]
                     ; for even
                     (for [x (range size)]
                       (for [y (range size)]
                         (if (>= x y) 1 0))))
-
          create-o (fn [size]
                     ; for odd
                     (for [x (range (inc (quot size 2)))]
                       (for [y (range size)]
                         (if (and (<= x y) (>= (- (dec size) x) y)) 1 0))))
 
+         ; rotation
          reverse-x (fn [coll]
                      (reverse coll))
          reverse-y (fn [coll]
@@ -82,22 +122,58 @@
          reverse-xy (fn[coll]
                       (reverse-y (reverse-x coll)))
 
-         check (fn [mask]
-                 (= (map #(bit-and %1 %2) rocks mask) mask))
+         shift-b (fn [x y mask]
+                   (debug-b (bit-shift-right mask (+ y (* x COLY)))))
 
-         countm (fn [mine]
-                  (reduce + (filter #(= 1 %) (flatten mine))))
+         harvestable? (fn [mask]
+                        (let [_maskb  (to-b (debug "mask" (padding mask)))
+                              _rocksb (to-b rocks)
+                              disx (- LOWX (count mask))
+                              disy (- COLY (count (first mask)))]
+                          (if (> 0 disx)
+                            false
+                            (some identity
+                              (for [i (range (inc disx))
+                                    j (range (inc disy))
+                                    :let [_shifted_maskb (shift-b i j _maskb)]]
+                                (= (bit-and _shifted_maskb _rocksb) _shifted_maskb))))))
 
-         harvest (fn [mask]
-                   (if (check mask)
-                     (countm mask)
-                     0))
+         search-e (fn [mask]
+                    ; check all pattern of rotation
+                    (or (harvestable? mask)
+                        (harvestable? (reverse-x mask))
+                        (harvestable? (reverse-y mask))
+                        (harvestable? (reverse-xy mask))))
+
+         ;main
+         harvest (fn []
+                   (loop [n LOWX]
+                     (let [mask-e (create-e n)]
+                       (if (search-e mask-e)
+                         (countm mask-e)
+                         (if (> n 2)
+                           (recur (dec n)))))
+                     ))
          ]
-     ;(debug rocks)
-     ;(countm rocks)
-     ;(harvest create-e)
+
+     (debug "rocks" rocks)
+     (harvest)
      ))
-  [15 15 15 15 15])
+  [0 31 0 31 0])
+
+
+(= 10 (__ [15 15 15 15 15]))
+(= 15 (__ [1 3 7 15 31]))
+(= 3 (__ [3 3]))
+(= 6 (__ [17 22 6 14 22]))
+
+; odd
+(= 4 (__ [7 3]))
+(= 9 (__ [18 7 14 14 6 3]))
+
+; nil
+(= nil (__ [21 10 21 10]))
+(= nil (__ [0 31 0 31 0]))
 
 ; 1. 1のみの最大と思しき三角形つくる
 ; 2. 答え合わせ
